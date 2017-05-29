5e6e9b9ee6b324fad444dba9f46bce305d79793c
sysintelligent <sysintelligent@gmail.com>
2017-05-28 17:21:49 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+var msg = undefined;
+
+msg = String(msg).trim();
+
+switch (msg) {
+    case "cf":
+        // cloud foundry
+        console.log('cf');
+        break;
+    case "neo":
+    default:
+        console.log('neo');
+}
