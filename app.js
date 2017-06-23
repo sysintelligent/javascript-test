80a2e8cb9e574f2e34ad4bf74223f6c41014a0f5
David Gu <d.gu@sap.com>
2017-06-23 09:07:57 -0700

    'Formatting'

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ var msg = undefined;
 msg = String(msg).trim();
 
 switch (msg) {
-    case "cf":
-        // cloud foundry
-        console.log('cf');
-        break;
-    case "neo":
-    default:
-        console.log('neo');
+  case "cf":
+    // cloud foundry
+    console.log('cf');
+    break;
+  case "neo":
+  default:
+    console.log('neo');
 }
